got dimensions to work, but wrong values

modules/attention.py
@@ -36,7 +36,7 @@ class CausalSelfAttention(nn.Module):
     masked_attention_score = attention_score + attention_mask
     softmax_attention = torch.nn.functional.softmax(masked_attention_score, dim=-1)
     multihead = torch.matmul(softmax_attention, value)
-    multihead = rearrange(multihead, 'b t h d -> b h (t d)')
+    multihead = rearrange(multihead, 'b h t d -> b t (h d)')
     return multihead
 
   def forward(self, hidden_states, attention_mask):

modules/gpt2_layer.py
@@ -44,7 +44,7 @@ class GPT2Layer(nn.Module):
            - A feed-forward layer that applies transformations to further refine the hidden states.
     """
 
-    ### YOUR CODE HERE
+    
     b4AttInput = self.attention_layer_norm(hidden_states)
     b4AttOutput = self.self_attention(b4AttInput, attention_mask)
     multiAttOutput = self.add(hidden_states, b4AttOutput, self.attention_dense, self.attention_dropout)